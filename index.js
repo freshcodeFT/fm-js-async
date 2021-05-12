3cb6d292e86de50ee808b951f8283f45e7dc0e2d
DeskYT <deskytube@gmail.com>
2021-05-12 10:38:28 +0300

    Add finally

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function createUserCards(data) {
   console.log(1);
 }*/
 
-function executor(resolve, reject) {
+/*function executor(resolve, reject) {
   resolve();
 }
 const myFirstPromise = new Promise(executor);
@@ -24,4 +24,16 @@ function delay(ms) {
 
 delay(5000).then(() => {
   console.log("success");
-});
+});*/
+
+let isFetching = true;
+function promiseCb(resolve, reject) {
+  setTimeout(()=>resolve("success"), 5000);
+}
+const mySecondPromise = new Promise(promiseCb);
+console.log(mySecondPromise);
+
+mySecondPromise
+.then((value) => console.log(value))
+.catch((error) => console.log(error))
+.finally((() => isFetching = false));
