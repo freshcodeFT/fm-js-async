262563f670afe08e7ea05adcc2e8ccda9f80d6e1
DeskYT <deskytube@gmail.com>
2021-05-12 11:20:46 +0300

    Add shchrodingerPromise

--- index.js
+++ index.js
@@ -26,14 +26,37 @@ delay(5000).then(() => {
   console.log("success");
 });*/
 
-let isFetching = true;
+/*let isFetching = true;
 function promiseCb(resolve, reject) {
-  setTimeout(()=>resolve("success"), 5000);
+  //reject('test');
+  setTimeout(()=>resolve("success"), 3000);
 }
 const mySecondPromise = new Promise(promiseCb);
 console.log(mySecondPromise);
 
 mySecondPromise
-.then((value) => console.log(value))
-.catch((error) => console.log(error))
-.finally((() => isFetching = false));
+  .then((value) => {
+    console.log(`Resolve: ${value}`);
+  })
+  .catch((error) => {
+    console.log(`Catch: ${error}`);
+  })
+  .finally(() => (isFetching = false));
+
+*/
+// [0,1)
+const shchrodingerPromise = new Promise((resolve, reject) => {
+  const catDestiny = Math.random();
+  if (catDestiny >= 0.5) {
+    resolve("Cat is alive");
+  } else {
+    reject("Cat is dead");
+  }
+});
+
+shchrodingerPromise.then(checkCat).catch(checkCat);
+console.log(1);
+
+function checkCat(str) {
+  console.log(`STATUS: ${str}`);
+}
