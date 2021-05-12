73d8fb6d6c637ccd122edc7195b638bb349bbd16
DeskYT <deskytube@gmail.com>
2021-05-12 10:02:32 +0300

    Add promise example

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const user = {
+/*const user = {
   firstName: "John",
   lastName: "Smith",
   isMale: true,
   isAgreed: undefined,
   age: 25,
   phones: ["0465489475734", "54639458349583", "435357284325"],
+  [Symbol("test")]: "value",
   get isAdult(){
-    return age >= 18;
+    return this.age >= 18;
   },
   getFullname(){
     return `${this.firstName} ${this.lastName}`;
@@ -16,4 +17,18 @@ const user = {
 const serializedObj = JSON.stringify(user);
 console.log(serializedObj);
 
-const restoredObj = JSON.parse(serializedObj);
+const restoredObj = JSON.parse(serializedObj);*/
+
+/*let i = 0;
+function foo(){
+  i++;
+  setTimeout(foo, 0);
+}*/
+
+fetch("./data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
