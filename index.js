8beeabd514a162b1d788bb0970dd6028eba0d94e
DeskYT <deskytube@gmail.com>
2021-05-13 10:40:35 +0300

    Add async function example

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-class UserError extends Error {
-  constructor(str) {
-    super(str);
-    this.userType = "user";
-    this.name = "User Error";
+async function getDataFromJson(path) {
+  const response = await fetch(path);
+  return await response.json();
+}
+
+async function asyncWriteData(path) {
+  try {
+    const data = await getDataFromJson(path);
+    document.body.append(JSON.stringify(data));
+  } catch (e) {
+    console.dir(e);
   }
 }
 
-try {
-  // обязателен
-  console.log('try1');
-  throw new UserError('Wrong user');
-  console.log('try2');
-} catch (e) {
-  // необязателен
-  console.log(e.userType);
-  console.log('catched error');
-  // но должен быть хотя бы один
-} finally {
-  // необязателен
-  console.log('finally');
+function syncWriteData(path) {
+  getDataFromJson(path)
+    .then((data) => document.body.append(JSON.stringify(data)))
+    .catch((e) => console.dir(e));
 }
