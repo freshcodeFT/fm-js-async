9d9d3fe08e4300c749cbd965ddc3c0c7c22a270c
DeskYT <deskytube@gmail.com>
2021-05-13 09:59:59 +0300

    Add custom err

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ const p = fetch("./data.json").then((response) => {
 .finally(() => {
   document.getElementById("load").remove();
 });
+
+class UserError extends Error {
+  constructor(str){
+    super(str);
+    this.userType = "user";
+    this.name = "User Error";
+  }
+}
+
+//console.dir(new UserError('Wrong password!!!'));
+
+function login(pass){
+  if(pass !== "123"){
+    throw new UserError("Wrong password!!!");
+  }
+}
+
+function auth() {
+  login('321');
+}
