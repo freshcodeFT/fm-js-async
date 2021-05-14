566ff78e4e97c5d85dd67a5956e7a30a7631779c
DeskYT <deskytube@gmail.com>
2021-05-14 11:18:34 +0300

    Add event delegation example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,23 @@
 "use strict";
 
-/**
- * Задание
- *
- * Напишите обработчик на событие click для кнопки, который при одинарном клике на кнопку будет выводить alert('Single'), 
- * а при двойном клике будет выводить alert('double').
- * При этом событие одинарного клика срабатывать не должно.
- */
+const listContainer = document.querySelector(".listContainer");
+listContainer.addEventListener("click", mainListHandler);
 
- const btn = document.getElementById("testButton");
- btn.addEventListener("click", createClickHandler());
 
- function createClickHandler() {
-  let timer = null;
-  let clicks = 0;
-  return (e) => {
-    clicks++;
-    if(clicks >= 2) {
-      clearTimeout(timer);
-      clicks = 0;
-      alert('double');
-      return;
-    }
+const list = document.querySelectorAll(".listContainer > li");
 
-    timer = setTimeout(()=>{
-      if(clicks === 1){
-        alert('single');
-      }
-      clicks = 0;
-    }, 250)
-
+function mainListHandler({ target }) {
+  const li = target.closest("li");
+  if(li){
+    li.innerHTML += "!";
   }
- }
+}
+
+const addButton = document.querySelector(".addButton");
+addButton.addEventListener("click", addBtnClickHandler);
+
+function addBtnClickHandler() {
+  const li = document.createElement("li");
+  li.innerText = "Added Item";
+  listContainer.append(li);
+}
